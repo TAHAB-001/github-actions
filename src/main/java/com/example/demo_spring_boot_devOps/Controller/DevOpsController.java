250ec54fa60818f2846cb0f364e7cb19d8a2bbbd
MASKAN <tahabohra1@gmail.com>
2025-08-20 16:15:04 +0530

    API CHANGE 01

--- src/main/java/com/example/demo_spring_boot_devOps/Controller/DevOpsController.java
+++ src/main/java/com/example/demo_spring_boot_devOps/Controller/DevOpsController.java
@@ -17,7 +17,7 @@ public class DevOpsController {
     private UserService userService;
     @GetMapping("get-data")
     public String getData(){
-        return "Hello DevOps at INDORE?";
+        return "Hello DevOps buddy?";
     }
 
     @PostMapping("save-user")
